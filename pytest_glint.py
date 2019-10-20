ff3cb049bafb3cf02d0792f874a6a67f100c4bf9
Eric Medina <eric.medina.atienza@gmail.com>
2019-10-20 20:21:53 +0800

    Add stubs for pathlib.Path and main.get_setting

--- pytest_glint.py
+++ pytest_glint.py
@@ -1,14 +1,45 @@
 # -*- coding: utf-8 -*-
 import shutil
+from pathlib import Path
 from unittest.mock import patch
 
 import pytest
+
+
+@pytest.fixture
+def path_exists():
+    patcher = patch.object(Path, 'exists', return_value=False)
+    yield patcher.start()
+    patcher.stop()
+
+
+@pytest.fixture
+def path_stub():
+    patcher = patch('pathlib.Path', autospec=Path)
+    _path_stub = patcher.start().return_value
+    _path_stub.exists.return_value = False
+    _path_stub.__truediv__.return_value = _path_stub
+    yield _path_stub
+    patcher.stop()
 
 
 @pytest.fixture(scope='session')
 def main():
     patcher = patch('glint.get_main')
     yield patcher.start().return_value
+    patcher.stop()
+
+
+@pytest.fixture
+def get_setting(request, main):
+    patcher = patch.object(main, 'get_setting')
+    gs = patcher.start()
+    if request.cls and hasattr(request.cls, 'settings'):
+        gs.side_effect = lambda x: request.cls.settings.get(x)
+    elif hasattr(request.module, 'settings'):
+        gs.side_effect = lambda x: request.module.settings.get(x)
+
+    yield
     patcher.stop()
 
 
